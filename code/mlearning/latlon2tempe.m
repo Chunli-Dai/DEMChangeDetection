e156db2fd6ec03667cc6bedced5111a9b690d3a5
Chunli Dai <dai.56@buckeyemail.osu.edu>
2025-01-01 21:01:24 -0500

    Add files via upload

--- code/mlearning/latlon2tempe.m
+++ code/mlearning/latlon2tempe.m
@@ -6,6 +6,7 @@ flagdata=2; %1, CRU TS (Climatic Research Unit gridded Time Series, v. 4.07) sur
 
 %flagmethod=2; %1, regular year interval; 2, use the actual data interval
 
+shift=365; %0, shift in days for correlation lag.
 
 if flagdata==1
     %Gettemperature.m years yeare tempe
@@ -46,7 +47,21 @@ elseif flagdata==2
     %2m temperature;
     ncFile='./era5/ERA5.nc'; %2008 to 2023, June July August September, hourly
     %Precipitation:2008 to 2023, April May June July August September, hourly
+    flagmon=0; % 1, montlhy data; 0 hourly data
     
+    if flagmethod==3
+%     ncFile='./era5/ERA5tempGreenland2010to22hr.nc';
+        
+        if flagmon==1
+%       ncFile='/Users/chunlidai/test/era5/monthly_temp.nc'; % lon 0 to 360
+      ncFile='/Users/chunlidai/test/era5/monthly_temppreci.nc'; % lon 0 to 360
+            if lonin<0; lonin=lonin+360;end
+        elseif flagmon==0
+      ncFile='/Users/chunlidai/test/era5/ERA5tempGreenland2009to13hr.nc';
+%       ncFile='/Users/chunlidai/test/era5/ERA5tempGreenland2014to19hr.nc';
+        end
+    end
+
     fileInfo = ncinfo(ncFile);
 
     %0.25 degrees
@@ -71,6 +86,10 @@ elseif flagdata==2
     yearv=yearv';
     elseif flagmethod==2
         yearv=Soi.year;% T1bin T2bin (datenum days).
+    elseif flagmethod==3
+         yearv=(timed)/365.25; %years from datenum %Original interval
+%           yearv=(floor(min(timed)):1:floor(max(timed)))/365.25; % daily interval, in unit of years
+
     end
     tempe_yr_med_n=nan(length(yearv),nall);
 
@@ -84,13 +103,26 @@ elseif flagdata==2
     
         %Temperature measured in kelvin can be converted to degrees Celsius (°C) by subtracting 273.15.
         %1440x121x46848, lon lat time
-        start  = [ilon ilat 1]; 
+        if 1
+        start  = [ilon ilat 1 ]; 
         count  = [1 1 Inf]; 
         stride = [1 1 1]; %Space between variable indices
-        tempe = ncread(ncFile, 't2m',start,count,stride);
+        elseif flagmon==1 %if flagmethod==3 % monthly data
+            % 1440 721 2 199        
+            start  = [ilon ilat 1 1]; 
+            count  = [1 1 1 Inf]; 
+            stride = [1 1 1 1]; %Space between variable indices
 
-        %get the mean of July August
-        
+        end
+        tempe = ncread(ncFile, 't2m',start,count,stride);
+        % Sep. 2024; scale and offset; already applied in ncread.m
+
+%         if flagmethod ==3 % hourly
+%             tempe_yr_med_n(:,i)=tempe(1,1,:); %hourly
+%             continue
+%         end
+                %get the mean of July August
+
         for k=1:length(yearv) %iyr=years:yeare
             iyr=yearv(k);
             Mtm=false(size(timeyr));
@@ -104,10 +136,17 @@ elseif flagdata==2
                 Mt=Mty&Mtm;
             elseif flagmethod ==2 
                 T1k=Soi.T1bin(k);T2k=Soi.T2bin(k);
-                Mt12=timed>=T1k&timed<=T2k;
+                Mt12=timed>=T1k-shift&timed<=T2k-shift;
                 Mt=Mt12&Mtm;
+            elseif flagmethod==3
+%                 Mt=floor(timed)==yearv(k)*365.25; % find all data within the selected day.
+                Mt=(timed)==yearv(k)*365.25; % original interval
             end
-            tempe_yr=tempe(1,1,Mt);
+            if flagmon==1 %4D
+                tempe_yr=tempe(1,1,1,Mt);
+            else %3D
+                tempe_yr=tempe(1,1,Mt);
+            end
             tempe_yr_med_n(k,i)=nanmean(tempe_yr(:));
         end
     end
